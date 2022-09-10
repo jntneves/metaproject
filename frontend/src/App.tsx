3d99c41efe0594834c025d0b40dad02fe0ca3ea8
Jonathan Neves <jow3327@gmail.com>
2022-09-09 23:37:03 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
   return (
-    <h1>Olá Mundo!</h1>
+    <>
+      <h1>Hello World!</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
