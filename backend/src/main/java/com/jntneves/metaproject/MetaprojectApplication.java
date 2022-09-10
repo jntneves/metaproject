8f8f5133202b4bb23ccabca8d1e0b8c55a44c5c4
Jonathan Neves <jow3327@gmail.com>
2022-09-09 23:09:36 -0300

    Project created

---
+++ backend/src/main/java/com/jntneves/metaproject/MetaprojectApplication.java
@@ -0,0 +1,13 @@
+package com.jntneves.metaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaprojectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jntneves/metaproject/MetaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jntneves.metaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
