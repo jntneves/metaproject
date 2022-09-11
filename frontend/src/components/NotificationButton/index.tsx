e88c746e8b4980ead1075ebd69cea45ba54c8558
Jonathan Neves <jow3327@gmail.com>
2022-09-10 21:04:46 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg';
 import { BASE_URL } from '../../utils/request';
 
@@ -10,11 +11,11 @@ type Props = {
 
 function handleClick(id: number) {
     axios(`${BASE_URL}/sales/${id}/notification`).then(response => {
-        console.log("success");
+        toast.info("O SMS foi enviado com sucesso!");
     })
 }
 
-function NotificationButton({saleId} : Props) {
+function NotificationButton({ saleId }: Props) {
     return (
         <div className="red-button" onClick={() => handleClick(saleId)}>
             <img src={icon} alt="Notificar" />
